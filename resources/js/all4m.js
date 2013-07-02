2a502e7db97a9f46c99d0e12d57ab98ff7a28791
Frank de Weger <frankdeweger@gmail.com>
2013-07-03 00:11:22 +0200

    Added console stub

--- resources/js/all4m.js
+++ resources/js/all4m.js
@@ -90,3 +90,27 @@ $(document).ready(function() {
         }
     });
 });
+
+// https://github.com/h5bp/html5-boilerplate/blob/a2b3d06e8b5b0bfb3bd608a1b938c9fa50b1c600/js/plugins.js
+// Avoid `console` errors in browsers that lack a console.
+(function() {
+    var method;
+    var noop = function () {};
+    var methods = [
+        'assert', 'clear', 'count', 'debug', 'dir', 'dirxml', 'error',
+        'exception', 'group', 'groupCollapsed', 'groupEnd', 'info', 'log',
+        'markTimeline', 'profile', 'profileEnd', 'table', 'time', 'timeEnd',
+        'timeStamp', 'trace', 'warn'
+    ];
+    var length = methods.length;
+    var console = (window.console = window.console || {});
+
+    while (length--) {
+        method = methods[length];
+
+        // Only stub undefined methods.
+        if (!console[method]) {
+            console[method] = noop;
+        }
+    }
+}());
